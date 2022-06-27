2406381dfb177ae2a8ca9c1e9bf0ce70a46db530
abdullahelwalid <abdallah.elmansoor@hotmail.com>
2022-06-28 00:42:11 +0800

    input component

---
+++ src/components/app.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Form from "./login";
+
+
+function App() {
+  return (
+    <dev>
+      <h1>its working?</h1>
+      <Form/>
+    </dev>
+  );
+};
+
+export default App;
