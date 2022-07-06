f929bc15389306555ee24a2701be8dfc54e12d21
abdullahelwalid <abdallah.elmansoor@hotmail.com>
2022-07-06 15:28:15 +0800

    added map function to note

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,110 +1,46 @@
 import React from "react";
 import Note from "./note";
 
-const content =
-  "is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+const notes = [
+  {
+    note:
+      "is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    id: 1,
+  },
+  {
+    note:
+      "is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    id: 2,
+  },
+  {
+    note:
+      "is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    id: 3,
+  },
+  {
+    note:
+      "is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    id: 4,
+  },
+  {
+    note:
+      "is simply dummy text of the printing and typesetting industry. ",
+    id: 5,
+  },
+];
 
 function Home() {
   return (
-    <div style={{width: "100%", height: "100%", textAlign: "center"}}>
+    <div style={{ width: "100%", height: "100%", textAlign: "center" }}>
       <h1 className="title">Welcome to INOTE</h1>
-      <div
-        className="
+      {notes.map((note) => (
+        <div
+          className="
             note-container"
-      >
-        <Note content={content} />
-      </div>
-      
-      <div
-        className="
-            note-container"
-      >
-      <Note content={content} />
-      </div>
-      <div
-        className="
-            note-container"
-      >
-      <Note content={content} />
-      </div>
-      <div
-        className="
-            note-container"
-      >
-      <Note content={content} />
-      </div>
-      <div
-        className="
-            note-container"
-      >
-      <Note content={content} />
-      </div>
-      <div
-        className="
-            note-container"
-      >
-      <Note content={content} />
-      </div>
-      <div
-        className="
-            note-container"
-      >
-      <Note content={content} />
-      </div>
-      <div
-        className="
-            note-container"
-      >
-      <Note content={content} />
-      </div>
-      <div
-        className="
-            note-container"
-      >
-      <Note content={content} />
-      </div>
-      <div
-        className="
-            note-container"
-      >
-      <Note content={content} />
-      </div>
-      <div
-        className="
-            note-container"
-      >
-      <Note content={content} />
-      </div>
-      <div
-        className="
-            note-container"
-      >
-      <Note content={content} />
-      </div>
-      <div
-        className="
-            note-container"
-      >
-      <Note content={content} />
-      </div>
-      <div
-        className="
-            note-container"
-      >
-      <Note content={content} />
-      </div>
-      <div
-        className="
-            note-container"
-      >
-      <Note content={content} />
-      </div>
-      <div
-        className="
-            note-container"
-      >
-        <Note content={content} />
-      </div>
+        >
+          <Note content={note.note} key={note.id} />
+        </div>
+      ))}
     </div>
   );
 }
